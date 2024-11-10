e70440851d96c3b7d17ef1f12019fcf495368362
Pitak Niyom <peterPaul@MacBook-Pro-khxng-Sutanya.local>
2024-11-10 21:19:46 +0700

    Peter : ex1, ex2 done

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,19 @@
 // Exercise #1: For Each Function
+let increaseFiveThousand = (num) => num + 5000;
 
 function forEach(array, operation) {
-  // Start coding here
+  let result = [];
+  for (let i in array){
+    result.push(operation(array[i]));
+  }
+  return result;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let newEmployeeSalaries = [];
 
 // Using `forEach` function here
+
+newEmployeeSalaries = forEach(employeeSalaries,increaseFiveThousand);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,26 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
+const checkPass = function(arr){
+  let scoreOver70 = 0;
+  for (let i of arr){
+      if (i > 70){
+        scoreOver70 +=1;}
+  }
+  if (scoreOver70 >= 5)
+    return true;
+  else
+    return false;
+}
+
+
+function atLeastFive(array, operation, classNumber) {
+  let scoreOver70 = operation(array);
+
+  if (scoreOver70){
+    console.log(`นักเรียนห้องที่ ${classNumber} ผ่านเกณฑ์ ✅`);
+  }else{
+    console.log(`นักเรียนห้องที่ ${classNumber} ไม่ผ่านเกณฑ์ ❌`);
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +29,6 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,checkPass,1);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2,checkPass,2);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3,checkPass,3);
